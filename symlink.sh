0a9b0e1eb950f35b05225cacfb4666cafb8205a8
zgrep <zgrep@users.noreply.github.com>
2016-03-25 00:23:54 -0400

    Comments as to what it does.
    Hey, you might want to know what it does without a bash reference
    nearby. :)

--- symlink.sh
+++ symlink.sh
@@ -3,10 +3,17 @@
 # Get all files.
 f=`ls -ap | grep -vE '\.md$|/$|^symlink\.sh$|^\.gitignore$'`;
 
+# For each file:
 for a in $f; do
+	# Get first line, get all text after the first space, replace ~ with $HOME
 	b=`sed '1q' "$a"`; b="${b#* }"; b="${b//\~/$HOME}";
+	# Get full path to it.
 	a=`pwd`/"$a";
+	# Hey, I wanna see what it's doing! :P
 	echo "$b -> $a";
+	# Remove if it exists.
 	if [[ -e "$b" ]]; then rm "$b"; fi;
+	# Create symlink. If you want it to copy, modify this line.
+	# Goes from $a, the local file, to $b, where we want it to go.
 	ln -s "$a" "$b";
 done;
